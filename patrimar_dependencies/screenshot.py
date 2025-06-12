7e9ee8467138f29e1a530091b2adad3170f5ad5d
Renan Brian Hermenegildo Oliveira <renan.oliveira@patrimar.com.br>
2025-06-12 11:49:26 -0300

    atualização

--- patrimar_dependencies/screenshot.py
+++ patrimar_dependencies/screenshot.py
@@ -1,18 +1,21 @@
 import os
 import pyautogui
 
-def screenshot(path:str=""):
-    if path:
-        if not path.lower().endswith('.png'):
-            path += ".png"
-    else:
-        path = os.path.join(os.getcwd(), 'screenshot')
-        if not os.path.exists(path):
-            os.makedirs(path)
-        path = os.path.join(path, 'screenshot.png')
-        
-    pyautogui.screenshot(path)
-    return path
+def screenshot(path:str="") -> str | None:
+    try:
+        if path:
+            if not path.lower().endswith('.png'):
+                path += ".png"
+        else:
+            path = os.path.join(os.getcwd(), 'screenshot')
+            if not os.path.exists(path):
+                os.makedirs(path)
+            path = os.path.join(path, 'screenshot.png')
+            
+        pyautogui.screenshot(path)
+        return path
+    except:
+        return None
         
     
 if __name__ == "__main__":
